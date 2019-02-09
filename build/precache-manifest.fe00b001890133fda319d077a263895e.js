40e67d1da687cc7333872ec5447d658d0feaa222
Gert Adamson <adamson.gert@hotmail.com>
2019-02-09 21:45:19 +0200

    Tehtävä 3.11

---
+++ build/precache-manifest.fe00b001890133fda319d077a263895e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f5e17d4188dcef3d9a01",
+    "url": "/static/js/main.f5e17d41.chunk.js"
+  },
+  {
+    "revision": "1bdeb50c48bfe976fdd4",
+    "url": "/static/js/1.1bdeb50c.chunk.js"
+  },
+  {
+    "revision": "2e631c3eaf71baff1ef9f0ded6295fa5",
+    "url": "/index.html"
+  }
+];
